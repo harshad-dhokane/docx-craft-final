68a702ae2860f67b49ff0b47a49b9b8b88ec9b1d
harshad-dhokane <worrk.harsshad@gmail.com>
2025-06-11 10:43:56 +0530

    vite production issue

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,13 @@ export default defineConfig({
         ]
       : []),
   ],
+  server: {
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'docxcraft.onrender.com'
+    ],
+  },
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
